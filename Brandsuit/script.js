e69d0b4be5b9f4eebc6f55c26c261ae06db72471
Rudasingwa Freddy <rudasingwafreddy343@gmail.com>
2024-10-16 11:52:32 +0200

    Hide Navigation bar on mobile screen

---
+++ Brandsuit/script.js
@@ -0,0 +1,5 @@
+// Function to toggle the visibility of the mobile navigation menu
+function toggleMenu() {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+}
